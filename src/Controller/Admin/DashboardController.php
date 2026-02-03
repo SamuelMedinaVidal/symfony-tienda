2abd4183681d19a8cf57fa518d2e86cdccf237b1
SamuelMedinaVidal <samumed3.0@gmail.com>
2026-02-03 09:35:10 +0000

    Corrección de errores y buena implementación de login y registro

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,27 +3,32 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Team;
+use App\Entity\Product;
 use Symfony\Component\HttpFoundation\Response;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+#[IsGranted('ROLE_ADMIN')]
 class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
 
-        return $this->redirect($adminUrlGenerator->setController(TeamCrudController::class)->generateUrl());
+        return $this->redirect($adminUrlGenerator->setController(ProductCrudController::class)->generateUrl());
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // Añadimos al menú el enlace al crud de Team
+        // Menú del crud de Product
+        yield MenuItem::linkToCrud('Product', 'fas fa-list', Product::class);
+        // Menú del crud de Team
         yield MenuItem::linkToCrud('Team', 'fas fa-list', Team::class);        
     }
 
--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -2,31 +2,29 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Team;
-use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_ADMIN')]
-class TeamCrudController extends AbstractCrudController
+class ProductCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return Team::class;
+        return Product::class;
     }
 
     public function configureFields(string $pageName): iterable
     {
-        return[
+        return [
             Field::new('name'),
             ImageField::new('photo')
-                ->setUploadDir('public/img')
-                ->setBasePath('/img'),
-            Field::new('designation')
+                ->setUploadDir('/public/img')
+                ->setBasePath('/img/'),
+            NumberField::new('price')->setNumDecimals(2)
         ];
     }
 }
